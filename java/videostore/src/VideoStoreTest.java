89c3006d7e5752ab0de301aee6bbd42bdd9ece44
Katerina <katerinaldg@gmail.com>
2018-01-02 14:31:06 +0000

    Apply refactoring to all tests
    - Inline values to remove duplication.

--- java/videostore/src/VideoStoreTest.java
+++ java/videostore/src/VideoStoreTest.java
@@ -14,41 +14,32 @@ public class VideoStoreTest extends TestCase {
 
     public void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
-        double amountOwed = 9.0;
-        int frequentRenterPoints = 2;
         customer.statement();
-        assertEquals(amountOwed, customer.getAmountOwed());
-        assertEquals(frequentRenterPoints, customer.getFrequentRenterPoints());
+        assertEquals(9.0, customer.getAmountOwed());
+        assertEquals(2, customer.getFrequentRenterPoints());
     }
 
     public void testDualNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
-        assertEquals("Rental Record for Fred\n" +
-                "\tThe Cell\t9.0\n" +
-                "\tThe Tigger Movie\t9.0\n" +
-                "You owed 18.0\n" +
-                "You earned 4 frequent renter points\n", customer.statement());
+        customer.statement();
+        assertEquals(18.0, customer.getAmountOwed());
+        assertEquals(4, customer.getFrequentRenterPoints());
     }
 
     public void testSingleChildrensStatement() {
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
-        assertEquals("Rental Record for Fred\n" +
-                "\tThe Tigger Movie\t1.5\n" +
-                "You owed 1.5\n" +
-                "You earned 1 frequent renter points\n", customer.statement());
+        customer.statement();
+        assertEquals(1.5, customer.getAmountOwed());
+        assertEquals(1, customer.getFrequentRenterPoints());
     }
 
     public void testMultipleRegularStatement() {
         customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
         customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
         customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
-
-        assertEquals("Rental Record for Fred\n" +
-                "\tPlan 9 from Outer Space\t2.0\n" +
-                "\t8 1/2\t2.0\n" +
-                "\tEraserhead\t3.5\n" +
-                "You owed 7.5\n" +
-                "You earned 3 frequent renter points\n", customer.statement());
+        customer.statement();
+        assertEquals(7.5, customer.getAmountOwed());
+        assertEquals(3, customer.getFrequentRenterPoints());
     }
 }
